c1dba0493bfd4513816acd968654558870724a9a
TastyLando <142912332+TastyLando@users.noreply.github.com>
2024-11-11 15:48:28 +0300

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace TürkiyeWeatherMap.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
